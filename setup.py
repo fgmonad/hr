01d487cfdcf1d1205ed3e030f392866601ab79c4
Felipe Gusmao <fgmonad@gmail.com>
2018-01-26 21:19:05 +0100

    Created wheel distribution

--- setup.py
+++ setup.py
@@ -7,9 +7,13 @@ setup(
     name='hr',
     version="0.1.0",
     description="CLI user management utility",
+    long_description=readme,
     author="Felipe Gusmao",
     author_email="fgmonad@gmail.com",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    install_requires=[],
+    entry_points={
+        'console_scripts': 'hr=hr.cli:main',
+    },
 )
