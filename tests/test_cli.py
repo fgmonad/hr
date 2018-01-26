7eaafcb95762bd4058ef289bc1e65814973bad29
Felipe Gusmao <fgmonad@gmail.com>
2018-01-26 21:18:25 +0100

    Implemented main CLI module

---
+++ tests/test_cli.py
@@ -0,0 +1,38 @@
+import pytest
+
+from hr import cli
+
+@pytest.fixture()
+def parser():
+    return cli.create_parser()
+
+def test_parser_fails_without_arguments(parser):
+    """
+    Without a path, the parser should exit with an error.
+    :param parser:
+    :return:
+    """
+    with pytest.raises(SystemExit):
+        parser.parse_args([])
+
+def test_parser_succeeds_with_a_path(parser):
+    """
+    With a path, the parser should exit with an error.
+    :param parser:
+    :return:
+    """
+    args = parser.parse_args(['/some/path'])
+    assert args.path == '/some/path'
+
+def test_parser_export_flag(parser):
+    """
+    The `export` value should default to False, but set
+    to True when passed to the parser.
+    :param parser:
+    :return:
+    """
+    args = parser.parse_args(['/some/path'])
+    assert args.export == False
+
+    args = parser.parse_args(['--export', '/some/path'])
+    assert args.export == True
